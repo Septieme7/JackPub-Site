f2ef256ad03f7bf04a2d78e93a2ff0db046a5c16
Septieme7 <lordstiko7@gmail.com>
2026-02-10 15:06:00 +0100

    First mise en place

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// Vous pouvez ajouter des animations ou interactions ici si besoin
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Image chargée en plein écran');
+});
